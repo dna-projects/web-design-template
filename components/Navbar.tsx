6e246049c9a63505132f5876e2aea73375ebe850
Andrew Finsand <andfinsand@gmail.com>
2024-05-27 21:33:45 -0700

    Navbar styling with responsive hamburger menu

---
+++ components/Navbar.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className='flex flex-row fixed w-full shadow-lg z-50'>
+
+                {/* Logo */}
+                    <Image
+                        src='/example-logo.png'
+                        alt=''
+                        width={150}
+                        height={150}
+                        priority={true}
+                        className='w-[55px] m-5'
+                    />
+
+                {/* Hamburger Menu */}
+                <div
+                    className='flex justify-end w-full lg:hidden'
+                    aria-label='mobile menu toggle'
+                >
+                    <button className='self-center h-[25px] m-5'>
+                        <Image
+                            src='/burger-menu.svg'
+                            alt=''
+                            width={25}
+                            height={25}
+                            priority={true}
+                            className=''
+                        />
+                    </button>
+                </div>
+
+                {/* Menu */}
+                <div className='hidden justify-center self-center w-full gap-16 lg:flex'>
+                        <a>
+                            Home
+                        </a>
+                        <a>
+                            About
+                        </a>
+                        <a>
+                            Services
+                        </a>
+                        {/* <a>
+                            Gallery
+                        </a>
+                        <a>
+                            Reviews
+                        </a> */}
+                        <a>
+                            Contact
+                        </a>
+                </div>
+
+            </nav>
+        </>
+    );
+}
