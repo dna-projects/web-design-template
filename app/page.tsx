5d34446374e853b39d1cc761276acc844b611169
Andrew Finsand <andfinsand@gmail.com>
2024-06-01 08:29:51 -0700

    Add About section on landing page with image.

--- app/page.tsx
+++ app/page.tsx
@@ -8,32 +8,47 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className='flex flex-col min-h-screen'>
+      <main className='flex flex-col'>
 
-        {/* Landing container */}
-        <div className='landing-image flex flex-col justify-center relative w-full h-screen'>
-          <div className='flex justify-center absolute bg-black opacity-55 w-full h-full'></div>
+          {/* Landing container */}
+          <div className='landing-image flex flex-col justify-center relative w-screen h-screen'>
+            <div className='flex justify-center absolute bg-black opacity-55 w-full h-full'></div>
 
-          {/* Content */}
-          <div className='flex flex-col self-center items-center z-20 px-5'>
-            <div className='text-center text-yellow-500 font-bold tracking-wider'>ELEVATE YOUR BUSINESS</div>
-            <h1 className='text-center text-[35px] text-white font-bold tracking-tighter max-w-[600px] mt-7 md:text-[45px]'>Unlock your Online Potential with Stunning Websites</h1>
-            <p className='text-center text-white mt-7'>Our talented designers craft pixel-perfect websites that enhance your brand&apos;s digital footprint</p>
+            {/* Content */}
+            <div className='flex flex-col self-center items-center z-20 px-5'>
+              <div className='text-center text-yellow-500 font-bold tracking-wider'>ELEVATE YOUR BUSINESS</div>
+              <h1 className='text-center text-[35px] text-white font-bold tracking-tighter max-w-[600px] mt-7 md:text-[45px]'>Unlock your Online Potential with Stunning Websites</h1>
+              <p className='text-center text-white mt-7'>Our talented designers craft pixel-perfect websites that enhance your brand&apos;s digital footprint</p>
+            </div>
+
+            {/* Buttons */}
+            <div className='flex flex-row justify-center gap-10 z-20 mt-14'>
+              <Link href='/about'>
+                <div className='border border-yellow-500 bg-yellow-500 font-bold px-7 py-3 hover:bg-white hover:border-white duration-200'>LEARN MORE</div>
+              </Link>
+              <Link href='/contact'>
+                <button className='border border-yellow-500 text-white font-semibold px-7 py-3 hover:bg-white hover:text-black hover:border-white duration-200 '>Get in Touch</button>
+              </Link>
+            </div>
           </div>
 
-          {/* Buttons */}
-          <div className='flex flex-row justify-center gap-10 z-20 mt-14'>
-            <Link href='/about'>
-              <div className='border border-yellow-500 bg-yellow-500 font-bold px-7 py-3 hover:bg-white hover:border-white duration-200'>LEARN MORE</div>
-            </Link>
-            <Link href='/contact'>
-              <button className='border border-yellow-500 text-white font-semibold px-7 py-3 hover:bg-white hover:text-black hover:border-white duration-200 '>Get in Touch</button>
-            </Link>
+          {/* About section */}
+          <div className='flex flex-col max-w-screen-2xl mx-auto gap-20 py-36 px-4 lg:flex-row 2xl:px-0'>
+            <Image
+              src='/about.jpg'
+              alt=''
+              width={1920}
+              height={1080}
+              priority={true}
+              className='self-center w-[450px] h-[252px] rounded-3xl shadow-xl lg:self-start 2xl:w-[750px] 2xl:h-[420px]'
+            />
+            <div className='flex flex-col'>
+              <div className='font-semibold tracking-wider'>ABOUT US</div>
+              <div className='text-[35px] font-bold md:text-[45px]'>Your Company Name</div>
+              <div className='mt-5'>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras tempus, nisi nec finibus efficitur, nulla nunc volutpat nulla, vel venenatis tellus nisi vel nulla. Sed vel nisi vel magna varius volutpat. Donec vel mauris vel nunc varius volutpat. Sed in nulla vel nunc venenatis volutpat. Sed vel nisi vel magna varius volutpat.</div>
+              <p className='mt-5'>Nulla facilisi. Sed vel eros vel velit posuere bibendum. Donec vel ex vel nunc faucibus pulvinar. Mauris eget enim vel nunc fringilla faucibus. Sed quis risus vel nunc venenatis volutpat. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+            </div>
           </div>
-        </div>
-
-        {/* About section */}
-        <div className='bg-white w-full h-[700px] text-center mt-20'>About Us</div>
 
       </main>
       <Footer />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,61 +10,62 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className='flex flex-row fixed w-full bg-white shadow-lg z-50'>
-
-                {/* Logo */}
-                    <Image
-                        src='/example-logo.png'
-                        alt=''
-                        width={150}
-                        height={150}
-                        priority={true}
-                        className='self-center w-[45px] h-[45px] m-4'
-                    />
-
-                {/* Hamburger Menu */}
-                <div
-                    className='flex justify-end w-full lg:hidden'
-                    aria-label='mobile menu toggle'
-                >
-                    <button className='self-center h-[25px] m-5'>
+            <nav className='flex flex-row justify-center fixed w-full bg-white shadow-lg z-50'>
+                <div className='flex flex-row w-full max-w-screen-2xl'>
+                    {/* Logo */}
                         <Image
-                            src='/burger-menu.svg'
+                            src='/example-logo.png'
                             alt=''
-                            width={25}
-                            height={25}
+                            width={150}
+                            height={150}
                             priority={true}
-                            className=''
+                            className='self-center w-[45px] h-[45px] m-4 2xl:w-[65px] 2xl:h-[65px] 2xl:mx-0'
                         />
-                    </button>
-                </div>
 
-                {/* Menu */}
-                <div className='hidden justify-center self-center w-full gap-16 lg:flex'>
-                    <Link href='/' className={`group flex flex-col ${currentPath === '/' ? 'active' : ''}`}>
-                        <div>Home</div>
-                        <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
-                    </Link>
-                    <Link href='/about' className={`group flex flex-col ${currentPath === '/about' ? 'active' : ''}`}>
-                        <div>About</div>
-                        <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/about' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
-                    </Link>
-                    <Link href='/services' className={`group flex flex-col ${currentPath === '/services' ? 'active' : ''}`}>
-                        <div>Services</div>
-                        <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/services' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
-                    </Link>
-                    {/* <Link href='/gallery' className={`group flex flex-col ${currentPath === '/gallery' ? 'active' : ''}`}>
-                        <div>Gallery</div>
-                        <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/gallery' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
-                    </Link>
-                    <Link href='/reviews' className={`group flex flex-col ${currentPath === '/reviews' ? 'active' : ''}`}>
-                        <div>Reviews</div>
-                        <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/reviews' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
-                    </Link> */}
-                    <Link href='/contact' className={`group flex flex-col ${currentPath === '/contact' ? 'active' : ''}`}>
-                        <div>Contact</div>
-                        <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/contact' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
-                    </Link>
+                    {/* Hamburger Menu */}
+                    <div
+                        className='flex justify-end w-full lg:hidden'
+                        aria-label='mobile menu toggle'
+                    >
+                        <button className='self-center h-[25px] m-5'>
+                            <Image
+                                src='/burger-menu.svg'
+                                alt=''
+                                width={25}
+                                height={25}
+                                priority={true}
+                                className=''
+                            />
+                        </button>
+                    </div>
+
+                    {/* Menu */}
+                    <div className='hidden justify-center self-center w-full gap-16 lg:flex'>
+                        <Link href='/' className={`group flex flex-col ${currentPath === '/' ? 'active' : ''}`}>
+                            <div>Home</div>
+                            <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
+                        </Link>
+                        <Link href='/about' className={`group flex flex-col ${currentPath === '/about' ? 'active' : ''}`}>
+                            <div>About</div>
+                            <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/about' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
+                        </Link>
+                        <Link href='/services' className={`group flex flex-col ${currentPath === '/services' ? 'active' : ''}`}>
+                            <div>Services</div>
+                            <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/services' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
+                        </Link>
+                        {/* <Link href='/gallery' className={`group flex flex-col ${currentPath === '/gallery' ? 'active' : ''}`}>
+                            <div>Gallery</div>
+                            <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/gallery' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
+                        </Link>
+                        <Link href='/reviews' className={`group flex flex-col ${currentPath === '/reviews' ? 'active' : ''}`}>
+                            <div>Reviews</div>
+                            <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/reviews' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
+                        </Link> */}
+                        <Link href='/contact' className={`group flex flex-col ${currentPath === '/contact' ? 'active' : ''}`}>
+                            <div>Contact</div>
+                            <div className={`h-0.5 bg-primary transition-width duration-300 ${currentPath === '/contact' ? 'w-full' : 'w-0 group-hover:w-full'}`}></div>
+                        </Link>
+                    </div>
                 </div>
 
             </nav>
