15b0dca34c8a752c3ee4f746df8cd0a66e42c244
Andrew Finsand <andfinsand@gmail.com>
2024-05-31 22:45:37 -0700

    Landing page image and content.

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,21 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-36">
-        <h1>Home Page</h1>
+      <main className='flex flex-col min-h-screen'>
+
+        {/* Landing image and content */}
+        <div className='landing-image flex justify-center relative w-full h-screen'>
+          <div className='flex justify-center absolute bg-black opacity-55 w-full h-full'></div>
+          <div className='flex flex-col self-center items-center z-20 px-5'>
+            <div className='text-center text-yellow-500 font-bold tracking-wider'>ELEVATE YOUR BUSINESS</div>
+            <h1 className='text-center text-[35px] text-white font-bold tracking-tighter max-w-[600px] mt-7 md:text-[45px]'>Unlock your Online Potential with Stunning Websites</h1>
+            <p className='text-center text-white  mt-7'>Our talented designers craft pixel-perfect websites that enhance your brand's digital footprint</p>
+          </div>
+        </div>
+
+        {/* About section */}
+        <div className='bg-white w-full h-[700px] text-center mt-20'>About Us</div>
+
       </main>
       <Footer />
     </>
